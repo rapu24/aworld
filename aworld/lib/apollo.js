a6dc19059e8f2690dc9eb065cd8dec27973fee45
rapu24 <124664582+rapu24@users.noreply.github.com>
2023-06-20 19:49:49 +0300

    Delete aworld directory

--- aworld/lib/apollo.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-
-export const client = new ApolloClient({
-  // uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
-  uri: `https://diycooking.blog/graphql`,
-  cache: new InMemoryCache(),
-});
